e6ca74d775153da11a84cc5a1974ded0daceed27
Neo <mingtseng.chang@huafu.com.tw>
2017-05-20 01:10:52 +0800

    1. use  GetBitmapTask to get BitMap 2. remove Thread & Handler 3. use GetOCRResultTask to Process OCR

--- app/src/main/java/com/mikewong/tool/tesseract/fragment/MainFragment.java
+++ app/src/main/java/com/mikewong/tool/tesseract/fragment/MainFragment.java
@@ -5,11 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -23,12 +20,12 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-import com.googlecode.tesseract.android.TessBaseAPI;
+import com.bumptech.glide.Glide;
 import com.mikewong.tool.tesseract.BuildConfig;
 import com.mikewong.tool.tesseract.R;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -37,50 +34,21 @@ import java.util.ArrayList;
 
 import idv.neo.utils.BitmapUtils;
 import idv.neo.utils.FolderFileUtils;
+import idv.neo.utils.GetBitmapTask;
+import idv.neo.utils.GetOCRResultTask;
 
 import static com.mikewong.tool.tesseract.C.PHOTO_CAPTURE;
 import static com.mikewong.tool.tesseract.C.PHOTO_RESULT;
-import static com.mikewong.tool.tesseract.C.SHOWRESULT;
-import static com.mikewong.tool.tesseract.C.SHOWTREATEDIMG;
 import static idv.neo.utils.FolderFileUtils.getSDPath;
 
 public class MainFragment extends Fragment {
     private static final String TAG = MainFragment.class.getSimpleName();
     private static String LANGUAGE = "eng";
     private static String IMG_PATH = getSDPath() + File.separator + BuildConfig.OCRIMAGEFOLDERNAME;
-
     private static TextView mResult;
     private static ImageView mSelectImage;
     private static ImageView ivTreated;
-
     private static CheckBox chPreTreat;
-
-    private static String textResult;
-    private static Bitmap bitmapSelected;
-    private static Bitmap bitmapTreated;
-
-
-    // 該handler用於處理修改結果的任務
-    public Handler mHandler = new Handler() {
-
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case SHOWRESULT:
-                    if (textResult.equals(""))
-                        mResult.setText(getString(R.string.identification_fail));
-                    else
-                        mResult.setText(textResult);
-                    break;
-                case SHOWTREATEDIMG:
-                    mResult.setText(getString(R.string.identificationings));
-                    showPicture(ivTreated, bitmapTreated);
-                    break;
-            }
-            super.handleMessage(msg);
-        }
-
-    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -136,33 +104,9 @@ public class MainFragment extends Fragment {
         return root;
     }
 
-    //    // 將圖片顯示在view中
-    private void showPicture(ImageView iv, Bitmap bmp) {
-        iv.setImageBitmap(bmp);
-    }
-
-    //    /**
-//     * 進行圖片識別
-//     *
-//     * @param bitmap   待識別?計?
-//     * @param language 識別語言
-//     * @return 識別結果字串
-//     */
-    public String doOcr(Bitmap bitmap, String language) {
-        TessBaseAPI baseApi = new TessBaseAPI();
-        baseApi.init(getSDPath(), language);
-        // 必須加此行，tess-two要求BMP必須為此配置
-        bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
-        baseApi.setImage(bitmap);
-        String text = baseApi.getUTF8Text();
-        baseApi.clear();
-        baseApi.end();
-        return text;
-    }
-
-    //    /**
-//     * 調用系統圖片編輯進行裁剪
-//     */
+    /**
+     * 調用系統圖片編輯進行裁剪
+     */
     public void startPhotoCrop(Uri uri) {
         Intent intent = new Intent("com.android.camera.action.CROP");
         intent.setDataAndType(uri, "image/*");
@@ -175,25 +119,10 @@ public class MainFragment extends Fragment {
         startActivityForResult(intent, PHOTO_RESULT);
     }
 
-    //    /**
-//     * 根據URI獲取點陣圖
-//     *
-//     * @return 對應的點陣圖
-//     */
-    private Bitmap decodeUriAsBitmap(Uri uri) {
-        Bitmap bitmap = null;
-        try {
-            bitmap = BitmapFactory.decodeStream(getContext().getContentResolver().openInputStream(uri));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return bitmap;
-    }
-
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        final Context context = getContext();
         if (resultCode == Activity.RESULT_CANCELED)
             return;
         if (requestCode == PHOTO_CAPTURE) {
@@ -202,37 +131,40 @@ public class MainFragment extends Fragment {
         }
         // 處理結果
         if (requestCode == PHOTO_RESULT) {
-            bitmapSelected = decodeUriAsBitmap(Uri.fromFile(new File(IMG_PATH, BuildConfig.OCRIMAGEFILENAME)));
-            if (chPreTreat.isChecked())
-                mResult.setText(getString(R.string.preprocess));
-            else
-                mResult.setText(getString(R.string.identificationings));
-            // 顯示選擇的圖片
-            showPicture(mSelectImage, bitmapSelected);
-
-            // 新線程來處理識別
-            new Thread(new Runnable() {
+            new GetBitmapTask(new GetBitmapTask.OnTaskCompleted() {
                 @Override
-                public void run() {
-                    if (chPreTreat.isChecked()) {
-                        bitmapTreated = BitmapUtils.doPretreatment(bitmapSelected);
-                        Message msg = new Message();
-                        msg.what = SHOWTREATEDIMG;
-                        mHandler.sendMessage(msg);
-                        textResult = doOcr(bitmapTreated, LANGUAGE);
-                    } else {
-                        bitmapTreated = BitmapUtils.converyToGrayImg(bitmapSelected);
-                        Message msg = new Message();
-                        msg.what = SHOWTREATEDIMG;
-                        mHandler.sendMessage(msg);
-                        textResult = doOcr(bitmapTreated, LANGUAGE);
-                    }
-                    Message msg2 = new Message();
-                    msg2.what = SHOWRESULT;
-                    mHandler.sendMessage(msg2);
-                }
+                public void onTaskCompleted(final Bitmap bitmap) {
 
-            }).start();
+                    if (chPreTreat.isChecked())
+                        mResult.setText(getString(R.string.preprocess));
+                    else
+                        mResult.setText(getString(R.string.identificationings));
+                    // 顯示選擇的圖片
+
+                    final ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    Glide.with(context).load(stream.toByteArray()).asBitmap().into(mSelectImage);
+                    Bitmap treated = BitmapUtils.doPretreatment(bitmap);
+                    mResult.setText(getString(R.string.identificationings));
+                    treated.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                    Glide.with(context).load(stream.toByteArray()).asBitmap().into(ivTreated);
+                    if (chPreTreat.isChecked()) {
+                        treated = BitmapUtils.doPretreatment(bitmap);
+                    } else {
+                        treated = BitmapUtils.converyToGrayImg(bitmap);
+                    }
+                    new GetOCRResultTask(new GetOCRResultTask.OnTaskCompleted() {
+                        @Override
+                        public void onTaskCompleted(String result) {
+                            Log.d(TAG, "Show  onTaskCompleted : " + result);
+                            if (result.equals(""))
+                                mResult.setText(getString(R.string.identification_fail));
+                            else
+                                mResult.setText(result);
+                        }
+                    }).execute(treated, LANGUAGE);
+                }
+            }).execute(context, new File(IMG_PATH, BuildConfig.OCRIMAGEFILENAME));
         }
     }
 
