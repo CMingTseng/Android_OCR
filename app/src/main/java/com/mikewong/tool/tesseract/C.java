fac00740a18b2e0bed1fed9fa4b30a3021d1f4f5
Neo <mingtseng.chang@huafu.com.tw>
2017-05-19 17:23:34 +0800

    add config

--- app/src/main/java/com/mikewong/tool/tesseract/C.java
+++ app/src/main/java/com/mikewong/tool/tesseract/C.java
@@ -7,4 +7,7 @@ package com.mikewong.tool.tesseract;
 public class C {
     public static final int PHOTO_CAPTURE = 0x11;// 拍照
     public static final int PHOTO_RESULT = 0x12;// 結果
+
+    public static final int SHOWRESULT = 0x101;
+    public static final int SHOWTREATEDIMG = 0x102;
 }
